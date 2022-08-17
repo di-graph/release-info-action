b32a60248511638510a6a9170e780f645b9070c8
Beneah Kombe <kombeneah@gmail.com>
2022-08-17 16:29:52 -0700

    Action Body [DG-1684]
    Adding logic into the Github action body that
    
    - Reads in the file path passed in from the invoker
    - Loads and parses the file contents as YAML
    - Sets the YAML properties into the action outputs.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-const holder = "Just so the file is created"
+const core = require('@actions/core');
+const { promises: fs } = require('fs');
+const path = require('path');
+const YAML = require('yaml');
+
+async function run() {
+    let filePath, fileContents;
+    try {
+        filePath = core.getInput('file_path');
+        core.debug(`filePath: ${filePath}`)
+
+        if (filePath) {
+            try {
+                fileContents =  await fs.readFile(path.resolve(filePath), 'utf-8');
+            } catch (error) {
+                console.error(error)
+                throw new Error(`The provided file path for Digraph release PR file may be incorrect. File is: ${filePath}`);
+            }
+        }
+
+        if (fileContents) {
+            // confirm it is valid YAML
+            try {
+                fileContents = YAML.parse(fileContents);
+            } catch (error) {
+                console.error('File contents is invalid YAML');
+                throw new Error('File contents is invalid YAML')
+            }
+        }
+
+        core.setOutput('repository', fileContents['repository'])
+        core.setOutput('new_major_version', fileContents['new_major_version'])
+        core.setOutput('new_minor_version', fileContents['new_minor_version'])
+        core.setOutput('new_patch_version', fileContents['new_patch_version'])
+        core.setOutput('previous_major_version', fileContents['previous_major_version'])
+        core.setOutput('previous_minor_version', fileContents['previous_minor_version'])
+        core.setOutput('previous_patch_version', fileContents['previous_patch_version'])
+    } catch (error) {
+        core.setFailed(error)
+    }
+}
+
+run();
