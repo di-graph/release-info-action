03f1e705424d19af7a495b57f530ee117ed988de
Beneah Kombe <kombeneah@gmail.com>
2022-08-23 12:44:08 -0700

    Simplify Action [DG-1741]
    Moving code around to simplify the flow of logic now that this is all
    working just fine.

--- src/index.js
+++ src/index.js
@@ -4,39 +4,29 @@ const path = require('path');
 const YAML = require('yaml');
 
 async function run() {
-    let filePath, fileContents;
     try {
+        let filePath, fileContents;
         filePath = core.getInput('file_path');
         core.debug(`filePath: ${filePath}`);
 
         if (filePath) {
-            try {
-                fileContents =  await fs.readFile(path.resolve(filePath), 'utf-8');
-            } catch (error) {
-                console.error(error);
-                throw new Error(`The provided file path for Digraph release PR file may be incorrect. File is: ${filePath}`);
-            }
-        }
+            fileContents =  await fs.readFile(path.resolve(filePath), 'utf-8');
+            fileContents = YAML.parse(fileContents);
+            core.debug(`fileContents: ${fileContents}`)
 
-        if (fileContents) {
-            // confirm it is valid YAML
-            try {
-                fileContents = YAML.parse(fileContents);
-            } catch (error) {
-                console.error(error);
-                throw new Error('File contents is invalid YAML')
-            }
+            core.setOutput('repository_name', fileContents['repository']);
+            core.setOutput('new_major_version', fileContents['new_major_version']);
+            core.setOutput('new_minor_version', fileContents['new_minor_version']);
+            core.setOutput('new_patch_version', fileContents['new_patch_version']);
+            core.setOutput('previous_major_version', fileContents['previous_major_version']);
+            core.setOutput('previous_minor_version', fileContents['previous_minor_version']);
+            core.setOutput('previous_patch_version', fileContents['previous_patch_version']);
+        } else {
+            throw new Error(`The provided file path for Digraph release PR file may be incorrect. File is: ${filePath}`);
         }
-
-        core.setOutput('repository_name', fileContents['repository'])
-        core.setOutput('new_major_version', fileContents['new_major_version'])
-        core.setOutput('new_minor_version', fileContents['new_minor_version'])
-        core.setOutput('new_patch_version', fileContents['new_patch_version'])
-        core.setOutput('previous_major_version', fileContents['previous_major_version'])
-        core.setOutput('previous_minor_version', fileContents['previous_minor_version'])
-        core.setOutput('previous_patch_version', fileContents['previous_patch_version'])
     } catch (error) {
-        core.setFailed(error)
+        console.log(error);
+        core.setFailed(error);
     }
 }
 
